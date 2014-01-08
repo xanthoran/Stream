8fdd160851a24a3fd2a3719680833b172863e416
Peter Leach <peter.g.leach@gmail.com>
2014-01-07 21:40:55 -0800

    Initial Commit

---
+++ StreamApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StreamApp
+//
+//  Created by PETER LEACH on 1/7/14.
+//  Copyright (c) 2014 PETER LEACH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "StreamAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StreamAppDelegate class]));
+    }
+}
